220cb5477f17e547b4aaa2d70ca116c0d338da04
Leandro Nishijima <4566637+leandronishijima@users.noreply.github.com>
2023-02-12 18:44:59 -0300

    mix new EggTimer

---
+++ egg_timer/lib/alarm.ex
@@ -0,0 +1,26 @@
+defmodule EggTimer.Alarm do
+  @moduledoc """
+  There are only three functions: a constructor to create new alarms, one that provides a function to call when  the alarm triggers, and the
+  default function for our alarm. We won't worry about adding alarms to our map of alarms within our core. We'll manage that map in our GenServer
+  """
+  defstruct ~w[duration name time f]a
+
+  def new(name, duration, f \\ &default_fn/0)
+      when is_atom(name) and is_integer(duration) and is_function(f) do
+    __struct__(
+      time: Time.utc_now(),
+      name: name,
+      duration: duration,
+      f: f
+    )
+  end
+
+  def trigger(alarm) do
+    alarm.f()
+    alarm
+  end
+
+  def default_fn do
+    IO.puts("Alarm triggered!")
+  end
+end
