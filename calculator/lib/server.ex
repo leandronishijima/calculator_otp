e110a9c8d9a67eb8fc92ebaf2cba8ea7eb46c27a
Leandro Nishijima <4566637+leandronishijima@users.noreply.github.com>
2023-02-12 21:07:09 -0300

    refactor: handle_call instead handle_cast (Calculator)

--- calculator/lib/server.ex
+++ calculator/lib/server.ex
@@ -10,28 +10,37 @@ defmodule Calculator.Server do
     {:ok, number}
   end
 
-  def handle_cast({:add, number}, state) do
-    {:noreply, Core.add(state, number)}
+  def handle_call({:add, number}, _from, state) do
+    result = Core.add(state, number)
+    {:reply, result, result}
   end
 
-  def handle_cast({:subtract, number}, state) do
-    {:noreply, Core.subtract(state, number)}
+  def handle_call({:subtract, number}, _from, state) do
+    result = Core.subtract(state, number)
+    {:reply, result, result}
   end
 
-  def handle_cast({:multiply, number}, state) do
-    {:noreply, Core.multiply(state, number)}
+  def handle_call({:multiply, number}, _from, state) do
+    result = Core.multiply(state, number)
+    {:reply, result, result}
   end
 
-  def handle_cast({:divide, number}, state) do
-    {:noreply, Core.divide(state, number)}
+  def handle_call({:divide, number}, _from, state) do
+    result = Core.divide(state, number)
+    {:reply, result, result}
   end
 
-  def handle_cast(:negate, state) do
-    {:noreply, Core.negate(state)}
+  def handle_call(:negate, _from, state) do
+    result = Core.negate(state)
+    {:reply, result, result}
   end
 
-  def handle_cast(:clear, _state) do
-    {:noreply, 0}
+  def handle_call(:state, _from, state) do
+    {:reply, state, state}
+  end
+
+  def handle_call(:clear, _state) do
+    {:reply, 0, 0}
   end
 
   def handle_info(:inc, state) do
@@ -39,17 +48,13 @@ defmodule Calculator.Server do
     {:noreply, new_state}
   end
 
-  def handle_call(:state, _from, state) do
-    {:reply, state, state}
-  end
-
-  def add(pid, number), do: GenServer.cast(pid, {:add, number})
-  def subtract(pid, number), do: GenServer.cast(pid, {:subtract, number})
-  def multiply(pid, number), do: GenServer.cast(pid, {:multiply, number})
-  def divide(pid, number), do: GenServer.cast(pid, {:divide, number})
-  def negate(pid), do: GenServer.cast(pid, :negate)
+  def add(pid, number), do: GenServer.call(pid, {:add, number})
+  def subtract(pid, number), do: GenServer.call(pid, {:subtract, number})
+  def multiply(pid, number), do: GenServer.call(pid, {:multiply, number})
+  def divide(pid, number), do: GenServer.call(pid, {:divide, number})
+  def negate(pid), do: GenServer.call(pid, :negate)
   def inc(pid), do: send(pid, :inc)
-  def clear(pid), do: GenServer.cast(pid, :clear)
+  def clear(pid), do: GenServer.call(pid, :clear)
 
   def state(pid) do
     GenServer.call(pid, :state)
