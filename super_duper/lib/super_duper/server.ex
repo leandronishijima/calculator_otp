92c98884f8ca847b4dfe8acb207d368cc2cb85d3
Leandro Nishijima <4566637+leandronishijima@users.noreply.github.com>
2023-02-16 13:20:39 -0300

    add: SuperDuper.Server

---
+++ super_duper/lib/super_duper/server.ex
@@ -0,0 +1,27 @@
+defmodule SuperDuper.Server do
+  use GenServer
+
+  alias SuperDuper.Core
+
+  def init(character) do
+    IO.puts("Starting #{character}")
+
+    {:ok, Core.info(character)}
+  end
+
+  def handle_cast(:die, state) do
+    raise "Boom"
+    {:noreply, state}
+  end
+
+  def handle_call(:say, _from, {_name, says} = state) do
+    {:reply, says, state}
+  end
+
+  def start_link(character) do
+    GenServer.start_link(__MODULE__, character, name: character)
+  end
+
+  def die(server), do: GenServer.cast(server, :die)
+  def say(server), do: GenServer.call(server, :say)
+end
